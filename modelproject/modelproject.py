297a8e9b3d252e47afce66839f5dab0e044f051f
Rstrobaek <Rasmus@strobaek.dk>
2024-05-02 13:28:36 +0200

    first_model

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,92 @@
 from scipy import optimize
+from types import SimpleNamespace
+import numpy as np
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
+class Solow:
 
-    """ 
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # Parameters for production function
+        par.alpha = 0.2    # Capital share
+        par.beta = 0.4     # Labor share
+        par.kappa = 0.2    # Land share
+        par.epsilon = 1 - par.alpha - par.beta - par.kappa  # Oil share
+
+        # Parameters for technology growth
+        par.g = 0.02        # Growth rate of technology
+        par.tech_init = 1
+
+        # Land
+        par.land_init = 1
+
+        # Oil
+        par.oil.init = 1   # Initial oil stock
+        par.s_E = 0.1      # Extraction rate of oil
+
+        # Parameters for labor growth
+        par.labor_init = 1
+        par.n = 0.01        # Growth rate of labor
+
+        # Parameters for capital accumulation
+        par.s_Y = 0.2         # Savings rate
+        par.delta = 0.05    # Depreciation rate
+
+
+    def tech_growth(self):
+        """Technical Growth"""
+
+        par = self.par
+        A0 = par.tech.init
+        A = (1+par.g)*A0
+
+        return A
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    def labor_growth(self):
+        """Labor Growth"""
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+        par = self.par
+        L0 = par.labor_init
+        L_next = (1+par.n)*L0
+
+        return L_next
     
-    return result
+    def oil_stock(self):
+        """Growth of stock of oil"""
+
+        par = self.par
+        R0 = par.oil.init
+
+        R_next = (1-par.s_E)*R0
+
+        return R_next
+    
+    def production(self, K, L):
+        """Production Function"""
+
+        par = self.par
+        X = par.land_init
+        E = par.oil.init
+
+        Y = K**par.alpha * (par.A * L)**par.beta*X**par.kappa*E**par.epsilon
+
+        return Y
+    
+    def capital_accumulation(self):
+        """Capital Accumulation"""
+        par = self.par
+
+        K_next = par.s*self.Y + (1-par.delta)*self.K
+
+        return K_next
+
+
+
+
+
+
+# Not used in this project
+# def solve_ss(sesult
